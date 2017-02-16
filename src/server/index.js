c61201ba9aaab4c0f33673d8773314a6cbcb3660
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-02-16 20:02:06 +0000

    Clarified why we do this

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+/**
+ * Separating this from the main server file, allows us to do full es6+ from as low level as possible.
+ * */
 require('babel-core/register')({
     presets: [
         'stage-0',
