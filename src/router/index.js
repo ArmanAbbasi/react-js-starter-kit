277cbd1caa5acd26b9547fdada4d738e5bc11f6c
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-02-16 19:46:25 +0000

    Let's handle 404s

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,12 @@ import { Router, Route, hashHistory } from 'react-router';
 
 import HomePage from '../views/HomePage';
 import Listing from '../views/Listing';
+import NotFound from '../views/NotFound';
 
 export default (
     <Router history={hashHistory}>
         <Route path="/" component={HomePage}/>
         <Route path="/listing" component={Listing}/>
+        <Route path="*" component={NotFound}/>
     </Router>
 );
