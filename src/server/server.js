9bf4fd94a16176cf4ab2e12a94152e20da760df0
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-02-16 10:37:43 +0000

    not needed dependency removed

--- src/server/server.js
+++ src/server/server.js
@@ -6,6 +6,11 @@ import zLib from 'zlib';
 import handlebars  from 'express-handlebars';
 import nodeJsx from 'node-jsx';
 
+import React from 'react';
+import { RoutingContext, match } from 'react-router';
+import { Provider } from 'react-redux';
+import createLocation from 'history/lib/createLocation';
+
 const ONE_YEAR_IN_MILLIS = 31557600000;
 const APP_PORT_NUM = process.env.PORT || 3000;
 const DISTRIBUTION_FOLDER = 'dist';
@@ -57,12 +62,68 @@ app.use(compression({
     level: zLib.Z_BEST_COMPRESSION
 }));
 
+const renderFullPage = (html, initialState) => {
+    return `
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Full Stack Web Developer based in London</title>
+        <link rel="stylesheet" type="text/css" href="/static/app.css">
+      </head>
+      <body>
+        <div id="root">${html}</div>
+        <script>
+          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}; 
+        </script>
+        <script src="/static/bundle.js"></script>
+      </body>
+    </html>
+  `;
+};
 
 /**
  * Do following with all incoming GET requests
  * */
-app.get('*', (req, res) => {
-    res.setHeader('Content-Type', 'text/javascript');
+// app.get('*', (req, res) => {
+//     res.setHeader('Content-Type', 'text/javascript');
+// });
+app.get('/*', function (req, res) {
+    const location = createLocation(req.url);
+
+    match({ routes, location }, (err, redirectLocation, renderProps) => {
+
+        if(err) {
+            console.error(err);
+            return res.status(500).end('Internal server error');
+        }
+
+        if(!renderProps)
+            return res.status(404).end('Not found');
+
+        const store = configureStore();
+
+        const InitialView = (
+            <Provider store={store}>
+                {() =>
+                    <RoutingContext {...renderProps} />
+                }
+            </Provider>
+        );
+
+        //This method waits for all render component promises to resolve before returning to browser
+        fetchComponentDataBeforeRender(store.dispatch, renderProps.components, renderProps.params)
+            .then(html => {
+                const componentHTML = React.renderToString(InitialView);
+                const initialState = store.getState();
+                res.status(200).end(renderFullPage(componentHTML,initialState));
+            })
+            .catch(err => {
+                console.log(err);
+                res.end(renderFullPage('',{}));
+            });
+
+    });
 });
 
 /**
