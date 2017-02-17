7855f9d1d457aa606b6dc1eccea1f6bfd8b840d6
Arman Abbasi <arman.abbasi@marksandspencer.com>
2017-02-17 11:21:15 +0000

    don't need precss, we have scss

--- src/config/postcss.config.js
+++ src/config/postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         require('postcss-smart-import')({ /* ...options */ }),
-        require('precss')({ /* ...options */ }),
         require('autoprefixer')({ /* ...options */ })
     ]
 };
